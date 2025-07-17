62a1d87c1059a990392178c146f4cc6b77ed5bd9
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-17 20:29:15 +0530

    Design changes

---
+++ 02static-page/src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main className="main">
+            <h1 className="heading">Fun facts about React</h1>
+            <ul className="ul-list">
+                <li className="ul-list-item">Was first released in 2013</li>
+                <li className="ul-list-item">Was originally created by Jordan Walke</li>
+                <li className="ul-list-item">Has well over 200K stars on GitHub</li>
+                <li className="ul-list-item">is maintained by Meta</li>
+                <li className="ul-list-item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ 02static-page/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <img src="src/react.svg" alt="react-logo" />
+                <span className="nav-list-item">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
