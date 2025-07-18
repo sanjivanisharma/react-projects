592c58fd3b6dcd6ccaf21c6beab24ce86503d387
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-18 18:19:02 +0530

    Initial commit

---
+++ 03travelJournal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="src/assets/globe.png" alt="GlobeImage" />
+            <span>my travel journal.</span>
+        </header>
+    )
+}
