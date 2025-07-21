29424cb10e2d402fdc257a7c4dcba1ec08d81337
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-21 13:15:57 +0530

    Refactored code

--- 03travelJournal/src/App.jsx
+++ 03travelJournal/src/App.jsx
@@ -5,15 +5,10 @@ import data from "./data"
 
 export default function App() {
 
-  const journalEntries = data.map((entries) => {
+  const journalEntries = data.map((entry) => {
     return <Entry
-      id={entries.id}
-      img={entries.img}
-      title={entries.title}
-      country={entries.country}
-      googleMapsLink={entries.googleMapsLink}
-      dates={entries.dates}
-      text={entries.text}
+      key={entry.id}
+      {...entry}
     />
   })
   
@@ -22,14 +17,6 @@ export default function App() {
       <Header />
       <main className="container">
         {journalEntries}
-        {/* <Entry
-          img={{ src: "src/assets/mountFuji.png", alt: "Mount Fuji" }}
-          title="Mount Fuji"
-          country="Japan"
-          googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-          dates="12 Jan, 2021 - 24 Jan, 2021"
-          text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        /> */}
       </main>
     </>
   )
