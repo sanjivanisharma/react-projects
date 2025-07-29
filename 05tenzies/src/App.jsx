aea5607c58f5b4bd31845bddbd26a291bc09d8d8
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-29 14:48:48 +0530

    [05tenzies] Added buttons

--- 05tenzies/src/App.jsx
+++ 05tenzies/src/App.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
+
 export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        isHeld: false,
+        value: Math.ceil(Math.random() * 6),
+        id: nanoid()
+      }))
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice())
+  }
+
+  const diceElements = dice.map((dieObj) => (
+    <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />
+  ))
+
   return (
-    <h1>App running</h1>
+    <main className="game-board">
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
+    </main>
   )
 }
