4279893095ecf0703fa8b0afaf095213de3674d4
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-29 18:05:20 +0530

    [05tenzies] Functionality changes

--- 05tenzies/src/App.jsx
+++ 05tenzies/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
+
 import Die from "./Die"
+
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice())
+  const [dice, setDice] = useState(() => generateAllNewDice())
+
+  const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
   function generateAllNewDice() {
     return new Array(10)
@@ -16,19 +21,35 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice())
+    setDice(oldDice => oldDice.map(die =>
+      die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+    ))
+  }
+
+  function hold(id) {
+    setDice(oldDice => oldDice.map((die) => (
+      die.id === id ? { ...die, isHeld: !die.isHeld } : die
+    )))
   }
 
   const diceElements = dice.map((dieObj) => (
-    <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
   ))
 
   return (
     <main className="game-board">
+      {gameWon && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={gameWon ? generateAllNewDice : rollDice}>{gameWon ? "New game" : "Roll"}</button>
     </main>
   )
 }
--- 05tenzies/src/Die.jsx
+++ 05tenzies/src/Die.jsx
@@ -4,6 +4,8 @@ export default function Die(props) {
     }
 
     return (
-        <button style={styles}>{props.value}</button>
+        <button
+            onClick={props.hold}
+            style={styles}>{props.value}</button>
     )
 }
